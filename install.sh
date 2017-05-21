eb5a8a3d12e21deb7df4287f4f4e2a0a15503826
yanjing-sisu <yanjing_sisu@sina.com>
2017-05-21 04:02:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ cd shadowsocks_qt5
 sudo apt-get install git -y
 
 sudo apt-get install qt5-qmake qtbase5-dev libbotan1.10-dev g++ -y
-git clone https://github.com/shadowsocks/libQtShadowsocks
+sudo git clone https://github.com/shadowsocks/libQtShadowsocks
 cd libQtShadowsocks
-dpkg-buildpackage -uc -us -b
+sudo dpkg-buildpackage -uc -us -b
 cd ..
 sudo dpkg -i libqtshadowsocks_*.deb
 sudo dpkg -i libqtshadowsocks-dev*.deb
 
 sudo apt-get install qt5-qmake qtbase5-dev libqrencode-dev libappindicator-dev libzbar-dev libbotan1.10-dev -y
-git clone https://github.com/shadowsocks/shadowsocks-qt5
+sudo git clone https://github.com/shadowsocks/shadowsocks-qt5
 cd shadowsocks-qt5
-dpkg-buildpackage -uc -us -b
+sudo dpkg-buildpackage -uc -us -b
 cd ..
 sudo dpkg -i shadowsocks-libqtshadowsocks_*.deb
 sudo dpkg -i shadowsocks-qt5_*.deb
